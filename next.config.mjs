0ff4f4ccc157caf8dd3d046d70a82bb2989860be
Israel <israelblenn@gmail.com>
2025-09-03 17:16:19 +1000

    resolving build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,25 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { withPayload } from '@payloadcms/next/withPayload'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true, // added to bypass persistent tyoe error in .next/types/app/(frontend)/(grid)/[id]/page.ts
+    // added to bypass persistent tyoe error in .next/types/app/(frontend)/(grid)/[id]/page.ts
+    ignoreBuildErrors: true,
   },
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    domains: [
-      process.env.NEXT_PUBLIC_SITE_URL, // e.g. 'cms.example.com'
-    ],
+    domains: [process.env.NEXT_PUBLIC_SITE_URL],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    config.resolve.alias['@payload-config'] = path.resolve(__dirname, 'src/payload.config.ts')
+    return config
   },
 }
 
