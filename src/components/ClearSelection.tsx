427956bddba1e92d26b438b89385b87ac4ceaa4a
Israel <israelblenn@gmail.com>
2025-10-23 14:28:00 +1100

    fixing act selection glitch

--- src/components/ClearSelection.tsx
+++ src/components/ClearSelection.tsx
@@ -1,14 +1,19 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useSelectedAct } from '@/contexts/SelectedActContext'
 
 export default function ClearSelection() {
   const { setSelectedActId } = useSelectedAct()
+  const hasClearedRef = useRef(false)
 
   useEffect(() => {
-    setSelectedActId(null)
-  }, [setSelectedActId])
+    // Only clear once when the component first mounts
+    if (!hasClearedRef.current) {
+      setSelectedActId(null)
+      hasClearedRef.current = true
+    }
+  }, []) // Empty dependency array since we only want this to run once
 
   return null
 }
