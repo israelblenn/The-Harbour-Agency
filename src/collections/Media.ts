20c333fd1cf1b3de79f2ba6ede5381bf3da50606
Israel <israelblenn@gmail.com>
2025-10-23 17:04:33 +1100

    adding revalidation to media

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,9 +1,14 @@
 import type { CollectionConfig } from 'payload'
+import { revalidate } from '@/hooks/revalidate'
 
 export const Media: CollectionConfig = {
   slug: 'media',
   access: {
     read: () => true,
+  },
+  hooks: {
+    afterChange: [() => revalidate(['/vault', '/'])],
+    afterDelete: [() => revalidate(['/vault', '/'])],
   },
   fields: [
     {
